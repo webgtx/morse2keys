36e4e9bbe0630e17806efa8c7f621ee2700ceef6
webgtx <alx.zolotarov@gmail.com>
2023-01-27 15:48:29 +0000

    Update: config patch

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 from pynput import keyboard
 from pynput.keyboard import Key
-from time import perf_counter
+from time import sleep 
 
-timepoint = perf_counter()
-counter = 0 
+input_buffer = [None, None, None]
+controller = keyboard.Controller()
+mod_key = Key.ctrl_r
 
-def on_release(self):
-	global counter
-	global timepoint
+def combination2key(mod, option, key, timeout):
+	if input_buffer[1] == mod and input_buffer[0] == option:
+		print("emulate virtual key")
+		sleep(timeout)
+		controller.tap(key)
 
-	if self == Key.ctrl_r:
-		second = round(perf_counter() - timepoint)
-		counter += 1 
-		if counter > 2 or second > 2:
-			print(second, self)
-			counter = 0
-			timepoint = perf_counter()
+def on_press(self):
+	if len(input_buffer) >= 2: input_buffer.pop() 
+	input_buffer.insert(0, self)
 
-with keyboard.Listener(on_release=on_release) as l:
+	print(input_buffer)
+
+	# if self == Key.esc:
+	# 	exit(0)
+
+	combination2key(
+		mod=mod_key,
+		option=Key.alt_l,
+		key="Z",
+		timeout=.5
+	)
+
+	combination2key(
+		mod=mod_key,
+		option=Key.shift_l,
+		key="z",
+		timeout=.5
+	)
+
+	combination2key(
+		mod=mod_key,
+		option=Key.ctrl_l,
+		key="4",
+		timeout=.5
+	)
+
+with keyboard.Listener(on_press=on_press) as l:
 	l.join()
